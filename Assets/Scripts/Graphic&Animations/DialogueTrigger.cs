7c5dcc8ca8f37fbd38148ca1cc8a317f06e2c8eb
AbsoluteZer0527 <zhuluyuan200527@gmail.com>
2025-11-22 15:09:48 -0800

    Dialogue testing

--- Assets/Scripts/Graphic&Animations/DialogueTrigger.cs
+++ Assets/Scripts/Graphic&Animations/DialogueTrigger.cs
@@ -1,96 +1,104 @@
-// using UnityEngine;
-// using Yarn.Unity;
+using UnityEngine;
+using Yarn.Unity;
 
-// public class DialogueTrigger
-// {
-//     [Header("Dialogue Settings")]
-//     [SerializeField] private string startNode = "Start";
-//     [SerializeField] private bool canRepeat = false;
-//     [SerializeField] private bool autoStart = true;
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Dialogue Settings")]
+    [SerializeField] private string startNode = "Start";
+    [SerializeField] private bool canRepeat = false;
+    [SerializeField] private bool autoStart = true;
 
-//     private bool hasTriggered = false;
-//     private bool playerInRange = false;
+    [Header("Player Control")]
+    [SerializeField] private string playerTag = "Player";
 
-//     public void Interact(GameObject interactor)
-//     {
-//         // Only allow interaction if autoStart is false and player is in range
-//         if (!autoStart && playerInRange)
-//         {
-//             TryStartDialogue();
-//         }
-//     }
+    private bool hasTriggered = false;
+    private bool playerInRange = false;
+    private GameObject playerObject;
+    private MonoBehaviour[] playerScripts;
 
-//     private void OnTriggerEnter2D(Collider2D other)
-//     {
-//         if (other.CompareTag("Player"))
-//         {
-//             playerInRange = true;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            playerInRange = true;
+            playerObject = other.gameObject;
             
-//             // If autoStart is enabled, start dialogue immediately
-//             if (autoStart)
-//             {
-//                 TryStartDialogue();
-//             }
-//             // If autoStart is disabled, show interaction prompt or enable interaction
-//             else
-//             {
-//                 // You can add visual feedback here like showing an "E to interact" prompt
-//                 // For example: ShowInteractionPrompt(true);
-//             }
-//         }
-//     }
+            if (autoStart)
+            {
+                TryStartDialogue();
+            }
+        }
+    }
 
-//     private void OnTriggerExit2D(Collider2D other)
-//     {
-//         if (other.CompareTag("Player"))
-//         {
-//             playerInRange = false;
-            
-//             // Hide interaction prompt if autoStart is disabled
-//             if (!autoStart)
-//             {
-//                 // Hide visual feedback here
-//                 // For example: ShowInteractionPrompt(false);
-//             }
-//         }
-//     }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            playerInRange = false;
+        }
+    }
 
-//     private void TryStartDialogue()
-//     {
-//         // Check if we should prevent starting dialogue
-//         if (hasTriggered && !canRepeat) return;
+    private void Update()
+    {
+        // Manual trigger with E key if autoStart is disabled
+        if (!autoStart && playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            TryStartDialogue();
+        }
+    }
 
-//         var runner = FindObjectOfType<DialogueRunner>();
-//         if (runner != null && !runner.IsDialogueRunning)
-//         {
-//             // Disable player input before starting dialogue
-//             //TODO
+    private void TryStartDialogue()
+    {
+        if (hasTriggered && !canRepeat) return;
 
-//             runner.StartDialogue(startNode);
-//             hasTriggered = true;
+        var runner = FindObjectOfType<DialogueRunner>();
+        if (runner != null && !runner.IsDialogueRunning)
+        {
+            DisablePlayerInput();
+            runner.StartDialogue(startNode);
+            hasTriggered = true;
+            runner.onDialogueComplete.AddListener(OnDialogueComplete);
+        }
+    }
 
-//             // Re-enable input when dialogue ends
-//             runner.onDialogueComplete.AddListener(OnDialogueComplete);
-//         }
-//     }
+    private void DisablePlayerInput()
+    {
+        if (playerObject != null)
+        {
+            // Store all MonoBehaviours and disable them
+            playerScripts = playerObject.GetComponents<MonoBehaviour>();
+            foreach (var script in playerScripts)
+            {
+                if (script != null && script.enabled)
+                {
+                    script.enabled = false;
+                }
+            }
+        }
+    }
 
-//     private void OnDialogueComplete()
-//     {
+    private void EnablePlayerInput()
+    {
+        if (playerScripts != null)
+        {
+            foreach (var script in playerScripts)
+            {
+                if (script != null)
+                {
+                    script.enabled = true;
+                }
+            }
+        }
+    }
 
-//         // Clean up the listener
-//         var runner = FindObjectOfType<DialogueRunner>();
-//         if (runner != null)
-//         {
-//             runner.onDialogueComplete.RemoveListener(OnDialogueComplete);
-//         }
-//     }
-
-//     // Optional: Add visual feedback methods
-//     /*
-//     private void ShowInteractionPrompt(bool show)
-//     {
-//         // Implement visual feedback for interaction prompt
-//         // This could show/hide a UI element or change sprite opacity
-//     }
-//     */
-// }
+    private void OnDialogueComplete()
+    {
+        EnablePlayerInput();
+        
+        var runner = FindObjectOfType<DialogueRunner>();
+        if (runner != null)
+        {
+            runner.onDialogueComplete.RemoveListener(OnDialogueComplete);
+        }
+    }
+}
